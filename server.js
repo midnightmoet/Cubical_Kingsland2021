381543ba1ad10bc295612389b1cca0bc11eaeadc
[Lisa Miner] <midnightmoet@gmail.com>
2021-08-11 20:33:00 -0400

    restarted cubicle, don't ask why lol

---
+++ server.js
@@ -0,0 +1,22 @@
+const env = process.env.NODE_ENV || "development";
+const config = require("./config/config")[env];
+const app = require("express")();
+const mongoose = require("mongoose");
+require("./config/express")(app);
+require("./config/routes")(app);
+mongoose.connect(
+    "mongodb+srv://midnightmoet:Kingsland2021!@cubes.x1o99.mongodb.net/?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+    console.log("Database is CONNECTED! YAY!");
+});
+app.listen(
+    config.port,
+    console.log(`Listening on port ${config.port}! Now its up to you...`)
+);
